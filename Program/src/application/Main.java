228037e9d59f6ecbb7abf98843eee9b11b16a1dd
Gabriel Lima <lima.gabriel1990@gmail.com>
2025-10-03 03:09:44 -0300

    colocando main na package application

--- Program/src/application/Main.java
+++ Program/src/application/Main.java
@@ -1,8 +1,9 @@
+package application;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
