eb93afa05c4e4b213d58a77e145ee5b2423ba2ef
Gabriel Lima <lima.gabriel1990@gmail.com>
2025-10-03 03:07:47 -0300

    primeiro commit

---
+++ Program/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\augus\\IdeaProjects\\desafioCadastro1\\Program\\src\\formulario.txt");
+
+
+        try (BufferedReader leitor = new BufferedReader(new FileReader(file))) {
+            String linha = leitor.readLine();
+            while (linha != null) {
+                System.out.println(linha);
+                linha = leitor.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
