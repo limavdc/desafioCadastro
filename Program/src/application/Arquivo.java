cdebd83f6f04b853ec6e54a6596fb5538374aa3e
Gabriel Lima <lima.gabriel1990@gmail.com>
2025-10-03 19:52:48 -0300

    Leitura de arquivo "formulario.text"

--- Program/src/application/Arquivo.java
+++ Program/src/application/Arquivo.java
@@ -1,4 +1,36 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Arquivo {
+
+
+
+    public String ler(String caminhoArquivo) throws Exception {
+        String conteudo = "";
+
+
+        try (BufferedReader leitor = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha = leitor.readLine();
+
+            while (linha != null) {
+                conteudo += linha;
+                linha = leitor.readLine();
+
+                if(linha != null) {
+                    conteudo += "\n";
+                }
+            }
+        } catch (Exception erro) {
+            //System.out.println("Error: " + e.getMessage());
+            throw erro;
+        }
+
+        return conteudo;
+    }
 }
+
+
--- Program/src/application/Main.java
+++ Program/src/application/Main.java
@@ -1,26 +1,39 @@
 package application;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        File file = new File("C:\\Users\\augus\\IdeaProjects\\desafioCadastro1\\Program\\src\\formulario.txt");
+        String caminhoArquivo = "C:\\Users\\augus\\IdeaProjects\\desafioCadastro1\\Program\\src\\application\\formulario.txt";
 
+        //Arquivo arquivo = new Arquivo();
 
-        try (BufferedReader leitor = new BufferedReader(new FileReader(file))) {
-            String linha = leitor.readLine();
-            while (linha != null) {
-                System.out.println(linha);
-                linha = leitor.readLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+        /*
+        * Files.readAlllines -> para ler todas as linhas; quando for um arquivo pequeno
+        * BufferedReader -> complexo de escrever; ele é otimo para ler arquviso grande;
+        * quando precisa ler linha por linha,
+         */
+
+        try {;
+            Path caminhos = Paths.get(caminhoArquivo);
+            List<String> resultadoLista = Files.readAllLines(caminhos);
+
+            String conteudo = String.join("\n", resultadoLista);
+            System.out.println(conteudo);
+        } catch (Exception e) {
+           JOptionPane.showMessageDialog(null,"ERRO DO ARQUIVO");
+
         }
+
     }
 }
